1f17ca7f7856d7a533cf0f3aca0939bb6ba1a276
siqister <siqi.zhu@gmail.com>
2015-10-30 02:36:48 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,59 @@
+console.log("Assignment 4-B");
+
+var margin = {t:50,r:100,b:50,l:50};
+var width = document.getElementById('plot').clientWidth - margin.r - margin.l,
+    height = document.getElementById('plot').clientHeight - margin.t - margin.b;
+
+var canvas = d3.select('.canvas');
+var plot = canvas
+    .append('svg')
+    .attr('width',width+margin.r+margin.l)
+    .attr('height',height + margin.t + margin.b)
+    .append('g')
+    .attr('class','canvas')
+    .attr('transform','translate('+margin.l+','+margin.t+')');
+
+
+//Scales
+var scaleX = d3.scale.linear().domain([1960,2015]).range([0,width]),
+    scaleY = d3.scale.linear().domain([0,11000000]).range([height,0]);
+
+//Axis
+var axisX = d3.svg.axis()
+    .orient('bottom')
+    .scale(scaleX)
+    .tickFormat( d3.format('d') ); //https://github.com/mbostock/d3/wiki/Formatting
+var axisY = d3.svg.axis()
+    .orient('right')
+    .tickSize(width)
+    .scale(scaleY);
+
+//Draw axes
+plot.append('g').attr('class','axis axis-x')
+    .attr('transform','translate(0,'+height+')')
+    .call(axisX);
+plot.append('g').attr('class','axis axis-y')
+    .call(axisY);
+
+//Start importing data
+d3.csv('/data/world_bank_2012.csv', parse, dataLoaded);
+
+function parse(d){
+
+    //Eliminate records for which gdp per capita isn't available
+
+    //Check "primary completion" and "urban population" columns
+    //if figure is unavailable and denoted as "..", replace it with undefined
+    //otherwise, parse the figure into numbers
+    return {
+
+    };
+
+
+
+}
+
+function dataLoaded(error, rows){
+
+}
+
